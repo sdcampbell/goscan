8525ada6fa63acefa0ba9ae5aaf1d0ec72ee46e2
Steve Campbell <sdcampbell68@live.com>
2020-09-03 07:29:31 -0400

    Updated goscan.go to add text to the target usage.

--- goscan.go
+++ goscan.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	// command line args
 	portPtr := flag.String("ports", "21,22,23,25,53,80,81,88,110,111,123,137,138,139,143,161,389,443,445,500,512,513,548,623,624,1099,1241,1433,1434,1521,2049,2483,2484,3268,3269,3306,3389,4333,4786,4848,5432,5800,5900,5901,6000,6001,7001,8000,8080,8181,8443,10000,16992,16993,27017,32764", "TCP ports to scan, comma separated.")
-	targetPtr := flag.String("target", "scanme.nmap.org", "Target IP address or hostname")
+	targetPtr := flag.String("target", "scanme.nmap.org", "Target IP address, network address in CIDR format, or hostname")
 	flag.Parse()
 
 	var wg sync.WaitGroup
